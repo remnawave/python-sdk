ab4fe9d3996894432de19abe6d7833ec793c68c9
Artem <sm1kyx@gmail.com>
2025-04-24 19:50:41 +0200

    feat: Bump version to 1.0.5 in pyproject.toml; refactor UsersController to use default parameters for pagination

--- remnawave_api/controllers/users.py
+++ remnawave_api/controllers/users.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from rapid_api_client import Path, Query
+from rapid_api_client import Path
 from rapid_api_client.annotations import PydanticBody
 
 from remnawave_api.models import (
@@ -35,8 +35,8 @@ class UsersController(BaseController):
     @get("/users/v2", response_class=UsersResponseDto)
     async def get_all_users_v2(
         self,
-        size: Annotated[int, Query(100, description="Size of the page")],
-        start: Annotated[int, Query(0, description="Start index of the page")],
+        size: int = 100,
+        start: int = 0
     ) -> UsersResponseDto:
         """
         Get a paginated list of users.
